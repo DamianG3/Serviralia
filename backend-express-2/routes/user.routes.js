74fa27020d3454d9e449ace4827fd2e64a9fc3d6
Luz <luzvelascoreyes@gmail.com>
2025-10-27 04:17:27 -0500

    root GET methods controller + router

--- backend-express-2/routes/user.routes.js
+++ backend-express-2/routes/user.routes.js
@@ -47,5 +47,60 @@ const controller = require('../controllers/user.controller')
  */
 router.post('/review', upload.array('gallery'), controller.createReview)
 
+// ------------------------------------------ SKILLS ------------------------------------------
+
+/**
+ * @swagger
+ * /skills:
+ *  get:
+ *      summary: Searches for all skills
+ *      tags: [Search Page]
+ *      responses:
+ *          200:
+ *              description: OK
+ *          400:
+ *              description: Database error
+ */
+router.get('/skills', controller.getAllSkills)
+
+/**
+ * @swagger
+ * /SearchSkill/{id}:
+ *  get:
+ *      summary: Searches for all workers who have a specific skill
+ *      tags: [Search Page]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            description: Skill ID
+ *      responses:
+ *          200:
+ *              description: OK
+ *          400:
+ *              description: Database error
+ *              
+ */
+router.get('/SearchSkill/:id', controller.searchSkill)
+
+/**
+ * @swagger
+ * /profileinfo/{id}:
+ *  get:
+ *      summary: Returns all of the worker profile information
+ *      tags: [Worker Profile Page]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            description: Worker ID
+ *      responses:
+ *          200:
+ *              description: OK
+ *          400:
+ *              description: Database error
+ *          404:
+ *              description: Not Found
+ *              
+ */
+router.get('/profileinfo/:id', controller.getWorkerInfo)
 
 module.exports = router;
